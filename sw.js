bb7639217ff0fb28ef631ffed726f4581350ebd2
scarizard94 <p7mp4p4p5c@privaterelay.appleid.com>
2025-11-19 14:42:01 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+// Service Worker for FoodLog Pro
+const CACHE_NAME = 'foodlog-pro-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install event - cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // If both fail, could return an offline page
+        return caches.match('./index.html');
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
